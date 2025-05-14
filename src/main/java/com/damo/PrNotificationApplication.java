fa13d82bca8459f87d3af4a9209dc8a33d887f0f
hyun <apfhd0257@naver.com>
2025-05-14 13:44:08 +0900

    init

---
+++ src/main/java/com/damo/PrNotificationApplication.java
@@ -0,0 +1,13 @@
+package com.damo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/damo/PrNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
